8ef096aaab1b3e0a88f6e0e1633fa83972c0ffc8
Sam Bowditch <zctlsab@gmail.com>
2024-12-12 09:04:56 -0300

    att: build 3.9

--- src/hooks/use_window_size.tsx
+++ src/hooks/use_window_size.tsx
@@ -2,16 +2,18 @@ import { useState, useEffect } from 'react';
 
 // Hook
 function useWindowSize() {
-    // Initialize state with undefined width/height so server and client renders match
-    // Learn more here: https://joshwcomeau.com/react/the-perils-of-rehydration/
-    const [windowSize, setWindowSize] = useState({
-      width: 500,
-      height: 500,
-    });
-  
-    useEffect(() => {
-      // only execute all the code below in client side
-      // Handler to call on window resize
+  // Initialize state with undefined width/height so server and client renders match
+  // Learn more here: https://joshwcomeau.com/react/the-perils-of-rehydration/
+  const [windowSize, setWindowSize] = useState({
+    width: 500,
+    height: 500,
+  });
+
+  useEffect(() => {
+    // only execute all the code below in client side
+    // Handler to call on window resize
+
+    if (typeof window !== 'undefined') {
       function handleResize() {
         // Set window width/height to state
         setWindowSize({
@@ -19,17 +21,18 @@ function useWindowSize() {
           height: window.innerHeight,
         });
       }
-      
+
       // Add event listener
       window.addEventListener("resize", handleResize);
-       
+
       // Call handler right away so state gets updated with initial window size
       handleResize();
-      
+
       // Remove event listener on cleanup
       return () => window.removeEventListener("resize", handleResize);
-    }, []); // Empty array ensures that effect is only run on mount
-    return windowSize;
+    }
+  }, []); // Empty array ensures that effect is only run on mount
+  return windowSize;
 }
 
 export default useWindowSize;
